d89d4db7824e1d79251b8089cec0eca2065701b0
Divyani Sunil Chougale <67824869+divyanichougale548@users.noreply.github.com>
2024-03-04 15:44:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './components/Form';
+function App() {
+  return (
+    <div className="App">
+      <header > 
+        <Form/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
